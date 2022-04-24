db5353f9276a21a2e7811c446c34bbd08cb3c84d
Yap Feng Yuan <fengyuan.yap@gmail.com>
2022-04-24 19:56:32 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Feng Yuan Yap on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
